800c903d6cf22c40c91ae75c15c43b6c35e14687
Alexei <alexei199318@mail.ru>
2023-04-04 02:12:44 +0300

    feat: add adaptive for 768

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-console.log('Оценка за задание: x баллов');
+console.log('Задание слегка не доделано - доделаю до 18:00 04.04.2023');
 console.log('Мой diskord - gromotron5525#6640');
 console.log('Мой telegram - https://t.me/gromotron');
