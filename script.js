884dc57386659a06f85a331d541b70d23d9db67a
alexei199617 <44381914+alexei199617@users.noreply.github.com>
2023-04-04 18:04:06 +0300

    feat: add some code for 320px in main page

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-console.log('Задание слегка не доделано - доделаю до 18:00 04.04.2023');
+console.log('Задание слегка не доделано - доделаю до 22:00 04.04.2023');
 console.log('Мой diskord - gromotron5525#6640');
 console.log('Мой telegram - https://t.me/gromotron');
