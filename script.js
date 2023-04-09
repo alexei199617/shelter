a33174255a382bec3b0f599572f7422b9b5f63ae
Alexei <alexei199318@mail.ru>
2023-04-10 02:30:02 +0300

    feat: start 3 part

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-console.log('Задание слегка не доделано - доделаю до 22:00 04.04.2023');
+// console.log('');
 console.log('Мой diskord - gromotron5525#6640');
 console.log('Мой telegram - https://t.me/gromotron');
+
+const burgerBtn = document.querySelector('.burgerBtn');
+const burgerMenuBlock = document.querySelector('.burgerMenuBlock');
+
+
+burgerBtn.addEventListener('click', openBurger);
+function openBurger(event) {
+    burgerMenuBlock.classList.remove('dNone');
+    // setTimeout(function(){burgerMenu.style.transform = 'translateX(0px)'},100);
+}
+burgerMenuBlock.addEventListener('click', closeBurger);
+function closeBurger(event) {
+    if (event.target.classList == 'burgerMenuBlock') {
+        burgerMenuBlock.classList.add('dNone');
+    }
+}
