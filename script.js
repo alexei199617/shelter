faad92a7414b9e1cb3ded5f575c63306a1c4375d
alexei199617 <44381914+alexei199617@users.noreply.github.com>
2023-04-10 17:59:59 +0300

    feat: start part 2

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ console.log('Мой telegram - https://t.me/gromotron');
 const burgerBtn = document.querySelector('.burgerBtn');
 const burgerMenuBlock = document.querySelector('.burgerMenuBlock');
 const burgerMenu = document.querySelector('.burgerMenu');
-let scrollY;
+let randomNumArr = ['one', 'two', 'tree', 'four', 'five', 'six', 'seven', 'eight'];
+let newRandomNumArr = [];
 
-
+//активация и деактивация бургер меню (пункт 1)
 burgerBtn.addEventListener('click', openBurger);
 function openBurger(event) {
     if (event.target.classList == 'burgerBtn burgerBtnRotate') {
@@ -20,7 +21,6 @@ function openBurger(event) {
         setTimeout(function(){burgerMenu.classList.remove('burgerMenuRotate')},100);
     }
 }
-
 burgerMenuBlock.addEventListener('click', closeBurger);
 function closeBurger(event) {
     if (event.target.classList == 'burgerMenuBlock') {
@@ -38,5 +38,49 @@ document.querySelector('.navBar320').addEventListener('click', burgerLinkClick);
 function burgerLinkClick(event) {
     if (event.target.classList == 'navA' || event.target.classList == 'navA navAActive') {
         closeBurgerAction();
+    }
+}
+
+
+//перемешивание карточек (часть 2)
+function getNewArr() {
+    shuffle(randomNumArr);
+    addItemNewArr();
+}
+getNewArr()
+function shuffle(arr) {
+  arr.sort(() => Math.random() - 0.5);
+}
+function getNextArr() {
+    shuffle(randomNumArr);
+    addItemNewArr ();
+    addItemOldArr ();
+}
+function addItemNewArr () {
+    for (let i=0; i < 3; i++) {
+        newRandomNumArr.push(randomNumArr.pop())
+    };
+}
+function addItemOldArr () {
+    for (let i=0; i < 3; i++) {
+        randomNumArr.unshift(newRandomNumArr.shift())
+    }
+}
+// слайдер (часть 2)
+
+
+// пагинация pets (часть 3)
+const petsControllBlok = document.querySelector('.petsControllBlok');
+petsControllBlok.addEventListener('click', pageClick);
+function pageClick (event) {
+    let et = event.target;
+    if (et.classList.contains('arrStart') == true) {
+        return
+    } else if (et.classList.contains('arrPrev') == true) {
+        return
+    } else if (et.classList.contains('arrNext') == true) {
+        return
+    } else if (et.classList.contains('arrEnd') == true) {
+        return
     }
 }
